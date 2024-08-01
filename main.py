894fe4716d33cde573ea5406f221aa2210342764
AshDavid12 <gaashdavid@gmail.com>
2024-08-01 17:01:35 +0300

    using agents and tavily for web search

--- main.py
+++ main.py
@@ -1,52 +1,33 @@
-from langchain_cohere import ChatCohere
-from langchain_cohere import CohereRagRetriever
-from langchain_core.documents import Document
+from langchain.agents import AgentExecutor
+from langchain_cohere.chat_models import ChatCohere
+from langchain_cohere.react_multi_hop.agent import create_cohere_react_agent
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 import os
 
 load_dotenv('.env')
 
 cohere_api_key = os.getenv('COHERE_API_KEY')
-if not cohere_api_key:
-    raise ValueError("COHERE_API_KEY not found in environment variables")
+tavily_api_key = os.getenv('TAVILY_API_KEY')
+# Internet search tool - you can use any tool, and there are lots of community tools in LangChain.
+# To use the Tavily tool you will need to set an API key in the TAVILY_API_KEY environment variable.
+internet_search = TavilySearchResults(api_key= tavily_api_key)
 
-# Load the cohere chat model
-try:
-    cohere_chat_model = ChatCohere(cohere_api_key=cohere_api_key)
-except Exception as e:
-    print("Error initializing Cohere chat model:", e)
-    raise
+# Create and run the Cohere agent
+# Set a Cohere API key in the COHERE_API_KEY environment variable.
+llm = ChatCohere()
+agent = create_cohere_react_agent(
+    llm=llm,
+    tools=[internet_search],
+    prompt=ChatPromptTemplate.from_template("{question}"),
+)
+agent_executor = AgentExecutor(agent=agent, tools=[internet_search], verbose=True)
 
-# Create the cohere rag retriever using the chat model with the web search connector
-try:
-    rag = CohereRagRetriever(llm=cohere_chat_model, connectors=[{"id": "web-search"}])
-except Exception as e:
-    print("Error initializing Cohere RAG retriever:", e)
-    raise
-
-# Debugging: Check the models available
-try:
-    print("Listing available models...")
-    models = cohere_chat_model.client.models.list()
-    print("Available models:", models)
-except Exception as e:
-    print("Error listing models:", e)
-    raise
-
-# Invoke the RAG retriever
-try:
-    docs = rag.invoke("Who founded Cohere?")
-    # Print the documents
-    for doc in docs[:-1]:
-        print(doc.metadata)
-        print("\n\n" + doc.page_content)
-        print("\n\n" + "-" * 30 + "\n\n")
-    # Print the final generation
-    answer = docs[-1].page_content
-    print(answer)
-    # Print the final citations
-    citations = docs[-1].metadata['citations']
-    print(citations)
-except Exception as e:
-    print("Error invoking RAG retriever:", e)
-    raise
+response = agent_executor.invoke({
+    "question": "I want to write an essay. Any tips?",
+})
+# See Cohere's response
+print(response.get("output"))
+# Cohere provides exact citations for the sources it used
+print(response.get("citations"))
